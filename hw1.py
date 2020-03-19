de914148882d3f7408a03311f365398813530faf
MichalinaH <62225692+MichalinaH@users.noreply.github.com>
2020-03-19 13:30:53 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,7 +1,6 @@
 from typing import List
-from datetime import timedelta, date
-
-
+from datetime import datetime, date, timedelta
+import numpy as np
 import pandas as pd
 
 CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
@@ -13,62 +12,23 @@ This speeds up the tests significantly
 """
 confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
 
-def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
-    """
-    Returns confirmed infection cases for country 'Poland' given a date.
-    Ex.
-    >>> poland_cases_by_date(7, 3, 2020)
-    5
-    >>> poland_cases_by_date(11, 3)
-    31
-    :param year: 4 digit integer representation of the year to get the cases for, defaults to 2020
-    :param day: Day of month to get the cases for as an integer indexed from 1
-    :param month: Month to get the cases for as an integer indexed from 1
-    :return: Number of cases on a given date as an integer
-    """
-    
-    x = confirmed_cases.loc[confirmed_cases["Country/Region"]=="Poland"].iloc[-1][f"{month}/{day}/{year%2000}"]
-    return x
 
+def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
+        url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+        df = pd.read_csv(url, error_bad_lines=False)
+        result = df.loc[df["Country/Region"]=="Poland"][f"{month}/{day}/{year-2000}"].values[0]
+        return result
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
-    """
-    Returns the top 5 infected countries given a date (confirmed cases).
-    Ex.
-    >>> top5_countries_by_date(27, 2, 2020)
-    ['China', 'Korea, South', 'Cruise Ship', 'Italy', 'Iran']
-    >>> top5_countries_by_date(12, 3)
-    ['China', 'Italy', 'Iran', 'Korea, South', 'France']
-    :param day: 4 digit integer representation of the year to get the countries for, defaults to 2020
-    :param month: Day of month to get the countries for as an integer indexed from 1
-    :param year: Month to get the countries for as an integer indexed from 1
-    :return: A list of strings with the names of the coutires
-    """
-
-    daata = f"{month}/{day}/{year%2000}"
-    where = "Country/Region"
-    return list(confirmed_cases[[where,daata]].groupby(by=where).sum().sort_values(by=daata,ascending=False).head(5).index)
+        data=f"{month}/{day}/20"
+        result=confirmed_cases[[data,'Country/Region']]
+        result=result.groupby('Country/Region').sum()
+        result=result.sort_values(by=[data], ascending=False).head(5)
+        return result.index.tolist()
   
-# Function name is wrong, read the pydoc
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
-    """
-    Returns the number of countries/regions where the infection count in a given day
-    was NOT the same as the previous day.
-    Ex.
-    >>> no_new_cases_count(11, 2, 2020)
-    35
-    >>> no_new_cases_count(3, 3)
-    57
-    :param day: 4 digit integer representation of the year to get the cases for, defaults to 2020
-    :param month: Day of month to get the countries for as an integer indexed from 1
-    :param year: Month to get the countries for as an integer indexed from 1
-    :return: Number of countries/regions where the count has not changed in a day
-    """
-    
-    given = date(year, month, day)
-    previous = given - timedelta(days=1)
-    given_new = f"{month}/{day}/{year % 2000}"
-    previous_new = f"{previous.month}/{previous.day}/{previous.year % 2000}"
-
-    x = len(confirmed_cases[confirmed_cases[given_new] != confirmed_cases[previous_new]])
-    return x
+        data=f"{month}/{day}/{year-2000}"
+        thedata=date(year,month,day)
+        prevday= thedata - timedelta(days=1)
+        pdata=prevday.strftime('%-m/%-d/%y')
+        return int(confirmed_cases[confirmed_cases[data] != confirmed_cases[pdata]][data].count())
