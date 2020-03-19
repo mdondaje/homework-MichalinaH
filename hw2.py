5686e3dd393ab440de53e505d07cc7ec6fab8d62
MichalinaH <62225692+MichalinaH@users.noreply.github.com>
2020-03-19 13:31:39 +0100

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -33,15 +33,20 @@ def countries_with_no_deaths_count(date: datetime.date) -> int:
     where infections were found, but nobody died on a given date. (DO NOT GROUP BY)
     Ex.
     >>> countries_with_no_deaths_count(datetime.date(2020, 3, 15))
-    171
+    44
     >>> countries_with_no_deaths_count(datetime.date(2020, 2, 18))
-    46
+    33
     :param date: Date object of the date to get the results for
     :return: Number of countries with no deaths but with active cases on a given date as an integer
     """
     
     # Your code goes here
-    pass
+    date=format_date(date)
+    counter=0
+    for i in dfC.index:
+      if(dfC.loc[i,date]>0 and dfD.loc[i,date]==0):
+        counter+=1
+    return counter
 
 
 def more_cured_than_deaths_indices(date: datetime.date) -> List[int]:
@@ -56,16 +61,19 @@ def more_cured_than_deaths_indices(date: datetime.date) -> List[int]:
     128, 154, 155, 156, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167,
     168, 169, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182,
     183, 184, 185, 187, 188, 189, 190, 191, 192, 193, 194, 202, 208]
-
     >>> more_cured_than_deaths_indices(datetime.date(2020, 2, 18))
     [0, 1, 2, 3, 4, 6, 7, 9, 10, 11, 12, 13, 15, 18, 19, 20, 92, 154, 156,
     157, 158, 159, 160, 161, 162, 163, 164, 166, 167, 168, 169, 171, 172,
     173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 187,
     188, 189, 190, 191, 192, 193, 194, 202, 347, 348, 403]
-
     :param date: Date object of the date to get the results for
     :return: A List of integers containing indices of countries which had more cured cases than deaths on a given date
     """
     
     # Your code goes here
-    pass
+    date = format_date(date)
+    list = []
+    for i in dfC.index:
+      if (dfR.loc[i , date] >  dfD.loc[i , date]):
+        list.append(dfC.index[i])
+    return list
